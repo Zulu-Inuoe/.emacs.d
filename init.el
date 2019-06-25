5a3b6ebd41f67f6cecd4d5e57b1b4fd90136fd29
Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
2019-06-25 13:26:50 -0400

    Fix typo in binding newline for mrepl

--- init.el
+++ init.el
@@ -633,7 +633,7 @@ directory too."
 (use-package sly
   ;; :if (package-installed-p 'sly)
   :bind (:map sly-mrepl-mode-map
-              ("<S-return>" . [remap newline]))
+              ("<S-return>" . newline))
   :custom
   (inferior-lisp-program "sbcl")
   (sly-command-switch-to-existing-lisp 'always)
